26cc94d655d0919a912c4643838bcfaff46d7a2e
BS885 <Bitya32885!>
2025-02-25 20:33:22 +0200

    first-Login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http:/localhost:3000/api/auth';
+  constructor(private http: HttpClient) { }
+  login(email: string, password: string): Observable<any> {
+    const body = { email, password }; 
+    return this.http.post<any>(`${this.apiUrl}/login`, body); 
+  }
+}
